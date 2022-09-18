60cbca01256c1ef2e4accad1cbe91a2b2ccec31c
loerllemit <loerllemit@gmail.com>
2022-09-18 10:57:11 +0800

    added gcp link to screener

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from home import views
 urlpatterns = [
     path("", views.home, name="home"),
     # path("about", views.about, name="about"),
-    path("contact", views.contact, name="contact"),
     path("resume", views.resume, name="resume"),
+    # path("screener", views.screener, name="screener"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,21 +4,18 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    # return HttpResponse("This is my homepage (/) ")
-    context = {"name": "Harry", "course": "Django"}
+    context = {"name": "Christian", "course": "Django"}
     return render(request, "home/home.html", context)
 
 
 def resume(request):
-    # return HttpResponse("This is my about page(/about) ")
     return render(request, "resume.html")
 
 
 def projects(request):
-    # return HttpResponse("This is my projects page (/projects) ")
     return render(request, "projects.html")
 
 
-def contact(request):
-    # return HttpResponse("This is my contact page (/contact) ")
-    return render(request, "contact.html")
+# def screener(request):
+## return HttpResponse("This is my contact page (/contact) ")
+## return render(request, "contact.html")
