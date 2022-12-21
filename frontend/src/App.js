7f3274fd137f72facaaacdc15c1dfe4f1e04da69
Christian Loer T. Llemit <christian.llemit@bld.ai>
2022-12-21 17:09:00 +0800

    fetch api data to react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const BASE_URL = "http://localhost:8000/api";
+
 function App() {
+   const [data, setData] = useState([]);
+
+   async function fetchData() {
+      let res = await fetch(BASE_URL);
+
+      try {
+         setData(await res.json());
+      } catch (err) {
+         console.log(err);
+      }
+   }
+
+   useEffect(() => {
+      fetchData();
+   }, []);
+
    return <div className="App bg-slate-600 min-h-screen"></div>;
 }
 
